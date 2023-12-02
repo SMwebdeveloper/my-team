d44b6bbf789dcd18d26c25315768605539bb7674
Samandar <samandarwebdeveloper@gmail.com>
2023-12-03 00:00:48 +0500

    add images and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: '#014e56',
+        primaryLight: '#f67e7e',
+        white: '#fff',
+        raptureBlue: '#79cbc7',
+        policeBlue: '#2c6269',
+        deepJungleGreen: '#004047',
+        sacramento: '#012f34',
+        darkGreen: '#002529'
+      }
+    },
+    
   },
   plugins: [],
 }
