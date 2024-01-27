c95e36b27850920e0ce5d1edaf713bb150729bfc
Samandar <samandarwebdeveloper@gmail.com>
2024-01-27 21:23:34 +0500

    fix add router animation

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
-  <navbar />
+  <div class="!scroll-smooth bg-primaryColor">
+    <navbar />
   <main>
-    <router-view />
+    <router-view v-slot="{Component}">
+      <transition name="bounce" made="out-in" :duration="550">
+        <Component :is="Component"/>
+      </transition>
+    </router-view>
   </main>
   <app-footer />
+
+  </div>
 </template>
 <script setup>
 import Navbar from "./components/Navbar.vue";
 import AppFooter from "./components/AppFooter.vue";
 </script>
 
-<style scoped></style>
+<style>
+main {
+  will-change: transform, opacity;
+}
+.bounce-enter-from,
+.bounce-leave-to {
+  opacity: 0;
+}
+.bounce-enter-active,
+.bounce-leave-active {
+  transition: opacity 0.3s ease-in-out;
+}
+</style>
