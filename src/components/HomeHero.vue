51beb0e77dce7a306db2027ac7d3ace67408b2d3
Samandar <samandarwebdeveloper@gmail.com>
2024-02-09 21:25:37 +0500

    fix

--- src/components/HomeHero.vue
+++ src/components/HomeHero.vue
@@ -6,17 +6,12 @@
       class="project-container flex flex-col pb-48 md:pb-64 lg:pb-72 lg:flex-row items-center justify-center lg:items-center lg:justify-between relative after:absolute after:bottom-[-100px] after:lg:right-0 after:w-[358px] after:h-[200px] after:bg-headerCircle2 after:bg-no-repeat"
     >
       <h2
-        v-motion
-        :initial="{ opacity: 0, y:-100 }"
-        :visibleOnce="{ opacity: 1, y:0, transition: { duration: 400} }"
         class="max-w-[635px] header-title mb-5 md:mb-6 lg:mb-0"
       >
         Find the <br />
         best <span class="text-primaryLight">talent</span>
       </h2>
-      <div v-motion
-        :initial="{ opacity: 0, x:100}"
-        :visibleOnce="{ opacity: 1, x:0, transition: { duration: 400,  } }"
+      <div
         class="max-w-[445px] lg:h-[195px] lg:flex lg:flex-col items-start justify-between"
       >
         <span class="hidden lg:inline-block bg-raptureBlue w-12 h-1"></span>
